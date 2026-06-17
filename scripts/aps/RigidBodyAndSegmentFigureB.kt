eaa73beb842406feb29185537f7d86dd06ee173f
Scott Michaels <smichaels@tmt.org>
2026-06-17 12:14:13 -0700

    new PEAS user interface support

--- scripts/aps/RigidBodyAndSegmentFigureB.kt
+++ scripts/aps/RigidBodyAndSegmentFigureB.kt
@@ -6,6 +6,15 @@ import esw.ocs.dsl.core.reusableScript
 val rigidBodyAndSegmentFigureB = reusableScript {
 
     onSetup("ask-user") { command ->
+
+        println("RigidBodyAndSegmentFigureB: ask-user start")
+        publishEvent(buildProcedureEvent(Prefix.apply(prefix),
+            type      = ProcedureEventType.INFO_MESSAGE,
+            dialogKey = "ask-user-start",
+            helpKey   = "help.ask-user",
+            messageId = "msg.ask-user.start"
+        ))
+
         println("RigidBodyAndSegmentFigureB: received ask-user")
         publishEvent(buildProcedureEvent(Prefix.apply(prefix), 
             type      = ProcedureEventType.USER_PROMPT,
